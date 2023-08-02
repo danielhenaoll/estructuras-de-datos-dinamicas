8cf444ea095d63472711353cb3fda4165c6a61b1
danielhenaoll <99374081+danielhenaoll@users.noreply.github.com>
2023-08-01 21:11:33 -0500

    Add files via upload

---
+++ Tarea 1- Estructuras de datos.py
@@ -0,0 +1,10 @@
+def sumar_a_maximo(num1, num2):
+    maximo = max(num1, num2)
+    resultado = maximo + num1 + num2
+    return resultado
+
+# Ejemplo de uso
+numero1 = 5
+numero2 = 10
+resultado = sumar_a_maximo(numero1, numero2)
+print("El resultado es:", resultado)
